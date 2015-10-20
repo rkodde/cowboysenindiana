281e4f9298c5f6e721a094f2ea238ce0993c88b4
Rene Kodde <renekodde@gmail.com>
2015-10-20 20:04:08 +0200

    splash screen and navigation drawer added

---
+++ CowboysenIndiana/app/src/main/java/nl/cowboysenindiana/app/rooster/cowboysenindiana/SplashActivity.java
@@ -0,0 +1,44 @@
+package nl.cowboysenindiana.app.rooster.cowboysenindiana;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+/**
+ * Created by Rene on 15-9-2015.
+ */
+public class SplashActivity extends Activity {
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView logo= (ImageView) findViewById(R.id.kdv_logo);
+        logo.setImageResource(R.drawable.kdv_logo);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing activity_splash screen with a timer. This will be useful when you
+             * want to show case your app kdv_logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, Dashboard.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
