9a6e83933f25acd25059ddeda9c09576fd0f46cb
Jerry <jjzeilmaker@gmail.com>
2015-11-22 14:54:49 +0100

    Changed set up Schedule. Day is now Object. Schedule is now modulair.
    \0/

--- CowboysenIndiana/app/src/main/java/nl/cowboysenindiana/app/schedule/ScheduleActivity.java
+++ CowboysenIndiana/app/src/main/java/nl/cowboysenindiana/app/schedule/ScheduleActivity.java
@@ -5,14 +5,13 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import nl.cowboysenindiana.app.frame.BaseActivity;
 import nl.cowboysenindiana.app.rooster.cowboysenindiana.R;
@@ -20,79 +19,42 @@ import nl.cowboysenindiana.app.rooster.cowboysenindiana.R;
 
 public class ScheduleActivity extends BaseActivity{
 
-    protected ScheduleAdapter adapter;
-
     @Override
     protected void goNext() {
 
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linid);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.scheduleLinearLayout);
 
         // Set actionBarTheme
         this.setActionBarTheme();
 
 
-        // add items to gridView programmatically by changing adapter dataset:
-        // -------------------------------------------------------
 
-        // make adapter for gridView
-        adapter = new ScheduleAdapter(this);
+// add your items, this can be done programatically
+        // your items can be from a database
 
-        // make new String[] to hold more data for dayDate
-        int oldLength = adapter.dayDate.length;
-        String[] dayDate_new = new String[oldLength+21];
-        // add old data to new String[]
-        for (int i = 0; i < oldLength; i++){
-            dayDate_new[i] = adapter.dayDate[i];
+        SimpleDateFormat sdf = new SimpleDateFormat("dd");
+
+        String date = sdf.format(new Date());
+
+        this.setActionBarTheme();
+
+
+
+        String leadSigned = "dd";
+
+        ScheduleObjectDay[] ObjectItemData = new ScheduleObjectDay[15];
+
+        for (int i = 0; i < ObjectItemData.length; i++) {
+            ObjectItemData[i] = new ScheduleObjectDay(date, leadSigned, 30);
         }
-        // add new data to new String[]
-        int p = 4;
-        for (int i = dayDate_new.length-1; i >= oldLength; i--){
-            dayDate_new[i] = String.valueOf(p);
-            p--;
-            if (p == 0){
-                p = 31;
-            }
-        }
-        // set new String[] as dayDate of adapater
-        adapter.dayDate = dayDate_new;
 
-        // do the same for entityLeadSigned and entityScheduled so we don't get array out of bounds error on those
-        oldLength = adapter.entityLeadSigned.length;
-        String[] entityLeadSigned_new = new String[oldLength+21];
-        // add old data to new String[]
-        for (int i = 0; i < oldLength; i++){
-            entityLeadSigned_new[i] = adapter.entityLeadSigned[i];
-        }
-        // add new data to new String[]
-        for (int i = dayDate_new.length-1; i >= oldLength; i--){
-            entityLeadSigned_new[i] = "Rahun";
-        }
-        // set new String[] as entityLeadSigned of adapter
-        adapter.entityLeadSigned = entityLeadSigned_new;
+        // our adapter instance
+        ScheduleAdapter adapter = new ScheduleAdapter(this, R.layout.schedule_grid_layout, ObjectItemData);
 
-        oldLength = adapter.entityScheduled.length;
-        String[] entityScheduled_new = new String[oldLength+21];
-        // add old data to new String[]
-        for (int i = 0; i < oldLength; i++){
-            entityScheduled_new[i] = adapter.entityScheduled[i];
-        }
-        // add new data to new String[]
-        for (int i = dayDate_new.length-1; i >= oldLength; i--){
-            entityScheduled_new[i] = "25 / 30";
-        }
-        // set new String[] as entityScheduled of adapter
-        adapter.entityScheduled = entityScheduled_new;
-
-
-        // notify adapter dataset has changed so it updates gridView
-        adapter.notifyDataSetChanged();
-
-        // ---------------------------------------------------------
-
-        // set up gridView
+        // create a new ListView, set the adapter and item click listener
         GridView gridView = new GridView(this);
-        //GridView gridView = (GridView) LayoutInflater.from(this).inflate(R.layout.schedule_grid_view, null);
         gridView.setAdapter(adapter);
+        adapter.setGroupColor("#756483");
         gridView.setBackgroundResource(R.drawable.schedule_border);
         gridView.setNumColumns(7);
         gridView.setClipChildren(false);
@@ -100,23 +62,35 @@ public class ScheduleActivity extends BaseActivity{
         gridView.setLayoutParams(new GridView.LayoutParams(GridLayout.LayoutParams.WRAP_CONTENT, GridLayout.LayoutParams.WRAP_CONTENT));
         linearLayout.addView(gridView);
 
+        // our adapter instance
+        ScheduleAdapter adapter2 = new ScheduleAdapter(this, R.layout.schedule_grid_layout, ObjectItemData);
 
-//        for(int i=1;i<=5;i++) {
-//            GridView gridView = new GridView(this);
-//            //GridView gridView = (GridView) LayoutInflater.from(this).inflate(R.layout.schedule_grid_view, null);
-//            gridView.setAdapter(new ScheduleAdapter(this));
-//            gridView.setBackgroundResource(R.drawable.schedule_border);
-//            gridView.setNumColumns(7);
-//            gridView.setClipChildren(false);
-//            gridView.setPadding(10, 5, 10, 5);
-//            gridView.setLayoutParams(new GridView.LayoutParams(GridLayout.LayoutParams.WRAP_CONTENT, GridLayout.LayoutParams.WRAP_CONTENT));
-//            linearLayout.addView(gridView);
-//        }
+        // create a new ListView, set the adapter and item click listener
+        GridView gridView2 = new GridView(this);
+        gridView2.setAdapter(adapter2);
+        adapter2.setGroupColor("#546564");
+        gridView2.setBackgroundResource(R.drawable.schedule_border);
+        gridView2.setNumColumns(7);
+        gridView2.setClipChildren(false);
+        gridView2.setPadding(10, 5, 10, 5);
+        gridView2.setLayoutParams(new GridView.LayoutParams(GridLayout.LayoutParams.WRAP_CONTENT, GridLayout.LayoutParams.WRAP_CONTENT));
+        linearLayout.addView(gridView2);
 
-        //this.generateSchedule();
-        //this.generateSchedule();
-        //this.generateSchedule();
 
+
+        // our adapter instance
+        ScheduleAdapter adapter3 = new ScheduleAdapter(this, R.layout.schedule_grid_layout, ObjectItemData);
+
+        // create a new ListView, set the adapter and item click listener
+        GridView gridView3 = new GridView(this);
+        gridView3.setAdapter(adapter3);
+        adapter3.setGroupColor("#997733");
+        gridView3.setBackgroundResource(R.drawable.schedule_border);
+        gridView3.setNumColumns(7);
+        gridView3.setClipChildren(false);
+        gridView3.setPadding(10, 5, 10, 5);
+        gridView3.setLayoutParams(new GridView.LayoutParams(GridLayout.LayoutParams.WRAP_CONTENT, GridLayout.LayoutParams.WRAP_CONTENT));
+        linearLayout.addView(gridView3);
 
     }
 
@@ -135,44 +109,6 @@ public class ScheduleActivity extends BaseActivity{
     public void generateSchedule(LinearLayout linearLayout){
         Integer scheduleDayLength = 7;
 
-        // Find existing linearLayout in xml by ID
-
-
-        // Create new GridView
-       // GridView gridView = new GridView(this);
-
-        // Set config
-        // Adapter is set for Data
-        //GridView gridView = (GridView) View.inflate(this, R.layout.schedule_grid_view, null);
-        GridView gridView = (GridView) LayoutInflater.from(this).inflate(R.layout.schedule_grid_view, null);
-        gridView.setAdapter(new ScheduleAdapter(this));
-        gridView.setBackgroundResource(R.drawable.schedule_border);
-
-
-
-
-
-//        gridView.setNumColumns(scheduleDayLength);
-//        gridView.setClipChildren(false);
-//        gridView.setPadding(10, 5, 10, 5);
-//
-//        gridView.setLayoutParams(new GridView.LayoutParams(GridLayout.LayoutParams.WRAP_CONTENT, GridLayout.LayoutParams.WRAP_CONTENT));
-
-//      	gridView.setColumnWidth(200);
-//     	gridView.setHorizontalSpacing(10);
-//    	gridView.setVerticalSpacing(10);
-
-        // Adding the gridview to the RelativeLayout as a child
-
-//
-//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view,
-//                                    int position, long id) {
-//                showToast("Item Clicked: " + position);
-//            }
-//        });
     }
 
     @Override
