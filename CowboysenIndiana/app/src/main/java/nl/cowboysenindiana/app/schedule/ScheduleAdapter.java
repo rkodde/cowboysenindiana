c71bf2f6036390bf8259ae5b7726ba65c04b6f4b
Jerry <jjzeilmaker@gmail.com>
2015-11-22 14:57:34 +0100

    Changed setup Schedule. Schedule now modulair. Day is now a object.

--- CowboysenIndiana/app/src/main/java/nl/cowboysenindiana/app/schedule/ScheduleAdapter.java
+++ CowboysenIndiana/app/src/main/java/nl/cowboysenindiana/app/schedule/ScheduleAdapter.java
@@ -1,172 +1,81 @@
 package nl.cowboysenindiana.app.schedule;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import nl.cowboysenindiana.app.rooster.cowboysenindiana.R;
 
 /**
- * Created by Tessa on 10/11/15.
+ * Created by Tessa on 22/11/15.
  */
 
-public class ScheduleAdapter extends BaseAdapter {
+public class ScheduleAdapter extends ArrayAdapter<ScheduleObjectDay> {
+
+private String groupColor = "#18A608";
+private String themeColor = "#658736";
+
     Context mContext;
+    int layoutResourceId;
+    ScheduleObjectDay data[] = null;
 
-    public void setShowDayName(boolean showDayName) {
-        this.showDayName = showDayName;
+    public ScheduleAdapter(Context mContext, int layoutResourceId, ScheduleObjectDay[] data) {
+
+        super(mContext, layoutResourceId, data);
+
+        this.layoutResourceId = layoutResourceId;
+        this.mContext = mContext;
+        this.data = data;
     }
-
-    boolean showDayName = true;
-
-
-
-    // Db Values6
-    private String groupColor = "#18A608";
-    private String themeColor = "#658736";
-
-    private String [] dayName = {"Ma","Di","Wo","Do","Vr","Za","Zo",
-            "","","","","","","",
-            "","","","","","","",
-            "","","","","","","",
-            "","","","","","","",};
-
-    protected String[] dayDate = {"30","1","2","3","4","5","6",
-            "7","8","9","10","11","12","13",
-            "14","15","16","17","18","19","20",
-            "21","22","23","24","25","26","27",
-            "28","29","31","1","2","3","14"};
-
-    private String entityLeadHead = "Teacher";
-    protected String[] entityLeadSigned ={"Rohit","Rahul","Ravi","Amit","Arun","","",
-            "Rohit","Rahul","Ravi","Amit","Arun","","",
-            "Rohit","Rahul","Ravi","Amit","Arun","","",
-            "Rohit","Rahul","Ravi","Amit","Arun","","",
-            "Rohit","Rahul","Ravi","Amit","Arun","",""};
-
-    private String entityScheduledHead = "Kids";
-    protected String [] entityScheduled ={"25 / 30","25 / 30","25 / 30","25 / 30","25 / 30","","",
-            "25 / 30","25 / 30","25 / 30","25 / 30","25 / 30","","",
-            "25 / 30","25 / 30","25 / 30","25 / 30","25 / 30","","",
-            "25 / 30","25 / 30","25 / 30","25 / 30","25 / 30","","",
-            "25 / 30","25 / 30","25 / 30","25 / 30","25 / 30","",""};
-
-
-
-    private LayoutInflater mInflater;
 
     public void setGroupColor(String groupColor){
         this.groupColor = groupColor;
     }
 
-    public ScheduleAdapter(Context c)
-    {
-        mContext = c;
-        mInflater = LayoutInflater.from(c);
-    }
-
-    public int getCount() {
-    //    return dayName.length;
-        return dayDate.length;
-    }
-
-    public Object getItem(int position)
-    {
-        return position;
-    }
-    public long getItemId(int position)
-    {
-        return position;
-    }
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-    ViewHolder holder = new ViewHolder();
-
-
-
-    if ( convertView == null )
-
-    {
-        convertView = mInflater.inflate(R.layout.schedule_grid_layout,
-                parent, false);
-        //holder = new ViewHolder();
-
-        // Gif the TextView custom settings.
-        // Day of the week
-        //holder.txtDayName = (TextView) convertView.findViewById(R.id.txtDayName);
-        //holder.txtDayName.setPadding(10, 10, 10, 10);
-
-        // Date of the day
-        holder.txtDayDate = (TextView) convertView.findViewById(R.id.txtDayDate);
-        holder.txtDayDate.setPadding(10, 10, 10, 10);
-
-
-        // Entity Lead Header
-        holder.txtEntityLeadHead = (TextView) convertView.findViewById(R.id.txtEntityLeadHead);
-        holder.txtEntityLeadHead.setBackgroundColor(Color.parseColor(groupColor));
-        //holder.txtEntityLeadHead.setBackgroundResource(R.drawable.schedule_border);
-
-        holder.txtEntityLeadHead.setPadding(10, 10, 10, 10);
-
-        // Entity Lead that dayName
-        holder.txtEntityLeadSigned = (TextView) convertView.findViewById(R.id.txtEntityLeadSigned);
-        holder.txtEntityLeadSigned.setPadding(10, 10, 10, 10);
-
-        // Entity Scheduled Header
-        holder.txtEntityScheduledHead = (TextView) convertView.findViewById(R.id.txtEntityScheduledHead);
-        holder.txtEntityScheduledHead.setBackgroundColor(Color.parseColor(groupColor));
-        holder.txtEntityScheduledHead.setPadding(10, 10, 10, 10);
-
-        // Entity Scheduled
-        holder.txtEntityScheduled = (TextView) convertView.findViewById(R.id.txtEntityScheduled);
-        holder.txtEntityScheduled.setPadding(10, 10, 10, 10);
-
-        //if (position == 0) {
-            convertView.setTag(holder);
-        //}
-
-    }
-
-    else
-
-    {
-            holder = (ViewHolder) convertView.getTag();
-    }
-
-
-
-        try {
-            // Set DB data to TextViewID
-            //holder.txtDayName.setText(dayName[position]);
-            holder.txtDayDate.setText(dayDate[position]);
-            holder.txtEntityLeadHead.setText(entityLeadHead);
-            holder.txtEntityLeadSigned.setText(entityLeadSigned[position]);
-            holder.txtEntityScheduledHead.setText(entityScheduledHead);
-            holder.txtEntityScheduled.setText(entityScheduled[position]);
-        }
-        catch (NullPointerException e){
-            Log.e("holder", e.toString());
+        if(convertView==null){
+            // inflate the layout
+            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+            convertView = inflater.inflate(layoutResourceId, parent, false);
         }
 
-        Log.d("pos", String.valueOf(position));
+        // object item based on the position
+        ScheduleObjectDay objectItem = data[position];
 
-    return convertView;
+        // get the TextView
+
+        // Day Date
+        TextView txtDayDate = (TextView) convertView.findViewById(R.id.txtDayDate);
+        txtDayDate.setText(String.valueOf(objectItem.itemDayDate));
+        //textViewItem.setTag(objectItem.itemId);
+
+        // Teachers Signed Header
+        TextView txtLeadSignedHead = (TextView) convertView.findViewById(R.id.txtEntityLeadHead);
+        txtLeadSignedHead.setBackgroundResource(R.drawable.schedule_border);
+        txtLeadSignedHead.setBackgroundColor(Color.parseColor(groupColor));
+        txtLeadSignedHead.setText("Teacher");
+
+        // Teachers Signed
+        TextView txtLeadSigned = (TextView) convertView.findViewById(R.id.txtEntityLeadSigned);
+        txtLeadSigned.setText(objectItem.itemLeadSigned);
+
+        // Kids Scheduled Head
+        TextView itemEntityScheduledHead = (TextView) convertView.findViewById(R.id.txtEntityScheduledHead);
+        itemEntityScheduledHead.setBackgroundResource(R.drawable.schedule_border);
+        itemEntityScheduledHead.setBackgroundColor(Color.parseColor(groupColor));
+        itemEntityScheduledHead.setText("Kids");
+
+        // Kids Scheduled
+        TextView itemEntityScheduled = (TextView) convertView.findViewById(R.id.txtEntityScheduled);
+        itemEntityScheduled.setText(String.valueOf(objectItem.itemEntityScheduled));
+
+        return convertView;
+
     }
-
-        static class ViewHolder
-    {
-        //TextView txtDayName;
-        TextView txtDayDate;
-        TextView txtEntityLeadSigned;
-        TextView txtEntityLeadHead;
-        TextView txtEntityScheduledHead;
-        TextView txtEntityScheduled;
-    }
-
 }
-
