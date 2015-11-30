067db52fcd5f46425fd878d57253188886957b8b
Jerry <jjzeilmaker@gmail.com>
2015-11-30 21:18:41 +0100

    Add models that where not added *pling*

--- CowboysenIndiana/app/src/main/java/nl/cowboysenindiana/app/model/EntryCorrection.java
+++ CowboysenIndiana/app/src/main/java/nl/cowboysenindiana/app/model/EntryCorrection.java
@@ -1,17 +1,18 @@
 package nl.cowboysenindiana.app.model;
 
+
 import java.sql.Date;
-import java.sql.Time;
 
 /**
  * Created by Tessa on 30/11/15.
  */
-public class Entry {
-    private int entry_id;
-    private int persons_id;
-    private int group_id;
-    private Date    date;
-    private int confirm;
+public class EntryCorrection extends Entry {
+
+    private int     entry_id;
+    private int     entry_correction_id;
+    private String  entry_correctioncol;
+    private int     type;
+    private int     confirm;
     private Date    time_arrival, time_departure;
     private Date    created_at, changed_at;
     private String  description;
--- CowboysenIndiana/app/src/main/java/nl/cowboysenindiana/app/model/PersonDay.java
+++ CowboysenIndiana/app/src/main/java/nl/cowboysenindiana/app/model/PersonDay.java
@@ -1,7 +1,7 @@
 package nl.cowboysenindiana.app.model;
 
-import java.sql.Time;
-import java.util.Date;
+
+import java.sql.Date;
 
 /**
  * Created by Tessa on 30/11/15.
@@ -10,6 +10,6 @@ public class PersonDay {
     private int group_id;
     private int person_id;
     private int day;
-    private Time    time_start, time_end;
+    private Date    time_start, time_end;
     private Date    date_start, date_end;
 }
